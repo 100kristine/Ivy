642aca5d0c8bf4232d6fcbd2c8878fea5995a1ff
Sean Murphy <smurph@berkeley.edu>
2015-04-13 18:47:31 -0700

    Revert "added notifications.js"
    This reverts commit 434aed825a354c66afcc44dab158b3333868210c.

--- IvyMoble/src/main.js
+++ IvyMoble/src/main.js
@@ -11,7 +11,6 @@ var CAL = require('calendar.js');
 var POWERTAB = require('powertab.js');
 var FLOWERS = require('myflowers.js');
 var LIGHTING = require('lighting.js');
-var NOTIFICATIONS = require('notifications.js');
 
 /*Screen stuff*/
 home = true;
@@ -178,7 +177,8 @@ var lightsButton = new MyButtonTemplate({url:"lights.png",name:"lights"});
 
 
 function makeHome(){
-	return NOTIFICATIONS.getColumn();
+	return new Column({name:"home", left:0, right:0, top:40, bottom:100, skin: new Skin({fill:"black"}), 
+				contents:[]});
 }
 
 function makeCal(){
@@ -219,3 +219,6 @@ mainColumn.add(homeScreen);
 application.behavior = new ApplicationBehavior();
 application.add(mainColumn);
 application.add(menu);
+
+
+// IVY - MOBILE APP	
