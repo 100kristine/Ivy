4b5978ea8d88076a4a3d3da4f9f176586c30c009
Sean Murphy <smurph@berkeley.edu>
2015-05-01 13:05:23 -0700

    cant figure out how to change pink to white container

--- IvyMobile/src/notifications.js
+++ IvyMobile/src/notifications.js
@@ -8,14 +8,22 @@ var SLIDERS = require('controls/sliders');
 
 
 
-/*Screen stuff*/
-home = true;
-currentScreen = "start";
+var SCROLLER = require('mobile/scroller');
+var POWERLEVEL = "0";
+var STATUS = "connecting...";
 
-var whiteSkin = new Skin( { fill:"white" } );
+var colorStyle = new Style({ color: 'white', font: '15px Avenir', horizontal: 'center', vertical: 'middle', });
+var whiteSkin = new Skin({fill:"#ea557b"});
 var greenSkin = new Skin({fill:"green"});
 var redSkin = new Skin({fill:"red"});
 var blueSkin = new Skin({fill:"blue"});
+var labelStyle = new Style( { font: "20px Avenir", color:"black" } );
+var textStyle = new Style( { font: "55px Avenir", color:"white" } );
+
+
+/*Screen stuff*/
+home = true;
+currentScreen = "start";
 
 
 
@@ -23,6 +31,42 @@ var blueSkin = new Skin({fill:"blue"});
 var blue = new Skin({fill:"blue"});
 var green = new Skin({fill:"#1eaf5f"});
 var black= new Skin({fill:"white"});
+
+function makeLabel(str,sty){
+  return new Label({left:0, right:0, string:str,style:sty});
+}
+
+
+
+Handler.bind("/delay", Object.create(Behavior.prototype, {
+  onInvoke: { value: 
+    function(handler, message) {
+      var query = parseQuery( message.query );
+        var duration = query.duration;
+        handler.wait( duration )
+    },
+  },
+  onComplete: { value: 
+    function(handler, message) {
+    },
+  },
+}));
+
+Handler.bind("/requestPower", Object.create(Behavior.prototype, {
+  onComplete: { value: 
+    function(handler, message, json) {
+      POWERLEVEL = json.power;
+      STATUS = json.status;
+      application.distribute("onDevicesChanged");
+    },
+  },
+  onInvoke: { value: 
+    function(handler, message) {
+      handler.invoke( new Message( "/delay?duration=500" ) );
+      handler.invoke( new Message("/getPowerLevel"), Message.JSON );
+    },
+  },
+}));
 
 var notifList = ["Water is half full", "Change your filter", "Add food to vase", "Water was filtered"];
 var lineList = [];
@@ -55,19 +99,12 @@ Handler.bind("/requestWater", Object.create(Behavior.prototype, {
 }));
 
 
-/* Seans changes */
-
-
-
 
 var l1 = new Style( { font: "25px Avenir", color:"#868786" } );
 var l2 = new Style( { font: "35px Avenir Black", color:"#1eaf5f", horizontal:"left"} );
 var l3 = new Style( { font: "70px Avenir", color:"white" } );
 var nStyle = new Style({font: "15px Avenir", color:"#868786"});
 
-function makeLabel(str,sty){
-	return new Label({top:2, left:0, right:0, string:str,style:sty});
-}
 var deleteButton = BUTTONS.Button.template(function($){ return{
 	 top:10, left:40,width:100,name:"off",skin: logoSkin, 
 	contents:[
@@ -95,8 +132,103 @@ function makeNotifs(){
 var notifications = new Column({left:0, top: 0, right:0, height: 500, skin:new Skin({fill:"white"}), name:"notifications",
 contents:[n]});
 
+
+var FINDER = -1000;
+
+var ChangingContainer = Container.template(function($) { return { top: -30,left: 40, width: (POWERLEVEL), height: 30, contents: [
+  Container($, { width: POWERLEVEL, height: 30, skin: $.skin, contents: [
+    //new Label({bottom: 2, left:2 ,width: 40, right:0, string: "%" + Math.round(POWERLEVEL,1), style: labelStyle}),
+  ], }),
+], }});
+
+var OuterContainer = Container.template(function($) { return { top: 50, width: 100, left: 40, height: 30, contents: [
+  Container($, { width: 100, height: 30, skin: new Skin( "black" ), contents: [
+  	new Label({bottom: 0, top: 0, left:-140, right:0, height: 40, string: "%" + Math.round(POWERLEVEL,1), style: labelStyle}),
+  ], }),
+], }});
+
+var TopContainer = Container.template(function($) { return { top: -250, bottom: -73, right: 165, width: 25, height: 15, contents: [
+  Container($, { top: 0, width: 10, height: 15, skin: $.skin, contents: [
+  ], }),
+], }});
+
+var MainContainer = Container.template(function($) { return { left: 0, right: 0, top: 0,  bottom: -140, skin: whiteSkin, contents: [
+    //new Label({top: 50, left:0, right:0, height: 40, string: "Battery Life:", style: textStyle}),
+    new Column({left:0, right:0, top:0, bottom:0,skin: new Skin({fill:"#1eaf5f"}),
+            contents:[
+              //makeLabel("power",l3), 
+              new Label({ left:0, right:0, top: 15, height: 40, string: "Power", style: textStyle, }),
+              //new Label({bottom: 170, left:0, right:0, height: 40, string: "Status: " + STATUS, style: textStyle, behavior: Object.create((MainContainer.behaviors[1]).prototype)}),
+            ]
+          }),
+     new Column({left:0, right:0, bottom:70,skin: whiteSkin,
+           contents:[
+           	  new Label({top: 0, bottom: 53, left:165, height: 40, string: "Status: " + STATUS, style: labelStyle, behavior: Object.create((MainContainer.behaviors[1]).prototype)}),
+          ]
+         }),
+    Column($, { left: 0, right: 0, top: 0, behavior: Object.create((MainContainer.behaviors[0]).prototype), }),
+], }});
+MainContainer.behaviors = new Array(2);
+MainContainer.behaviors[0] = Behavior.template({
+  onCreate: function(column, data) {
+    this.onDevicesChanged( column );
+  },
+  onDevicesChanged: function(column) {
+    column.empty();
+    this.build( column );
+  },
+  build: function(column) {
+  	//trace(STATUS + "\n");
+  	//STATUS = "Charging";
+    var topSkin = new Skin( "black" );
+    if (POWERLEVEL == "100") {
+      var topSkin = new Skin( "green" );
+      //STATUS = "Fully Charged";
+    }
+    var changingSkin = new Skin( "green" );
+    if (parseInt(POWERLEVEL) < 21) {
+      var changingSkin = new Skin( "red" );
+      //STATUS = "Low Battery";
+    }
+    FINDER = FINDER + 2;
+    //trace(FINDER + "\n");
+    column.add( new TopContainer( { skin: topSkin } )  );
+    column.add( new OuterContainer );
+    column.add( new ChangingContainer( { skin: changingSkin } ) );
+    //column.add( new Label({bottom: 170, left:0, right:0, height: 40, string: "Status: " + STATUS, style: textStyle}),);
+    
+  },
+})
+MainContainer.behaviors[1] = Behavior.template({
+  onDevicesChanged: function(column, content) {
+  //debugger
+    if (POWERLEVEL == "100") {
+      STATUS = "Fully Charged";
+    }
+    if (parseInt(POWERLEVEL) < 21) {
+      STATUS = "Low Battery";
+    }
+  	column.string ="Status: " + STATUS;
+  	//column.first.string = "string";
+  },
+
+})
+
+
+
+var mainContainer = new MainContainer();	
+	
+	
+	
+	
+	
+	
+	
+	
 	//var col = 
 var buttonArr = [];
+
+
 
 var column = new Column({left:0, right:0, top:0, bottom:0, skin:new Skin({fill:"white"}), contents:[new Column({name:"flower", left:0, right:0, top:0, bottom:100, skin: new Skin({fill:"white"}), 
 				contents:[
@@ -110,11 +242,15 @@ var column = new Column({left:0, right:0, top:0, bottom:0, skin:new Skin({fill:"
 					
 					//new Line({name:"labels", left:0, right:0, top:10, bottom:0, skin: new Skin({fill:"black"}),
 					//	contents:[makeLabel("None",l1),makeLabel("One",l1),makeLabel("Few",l1),makeLabel("Many",l1)]}),
+					//mainContainer,
 					notifications,
+					mainContainer,
+					//new mainContainer({ left:0, right:0, bottom:0, skin: new Skin({fill:"red"}),}),
+					
 					
 						
-				//	new Line({name:"padding", left:0, right:0, top:0, bottom:0, skin: new Skin({fill:"red"}),
-					//	contents:[]}),
+					//new Line({name:"padding", left:0, right:0, top:0, bottom:0, skin: new Skin({fill:"red"}),
+						//contents:[]}),
 				
 				]})], behavior: Object.create(Behavior.prototype, {
 	onCreate: { value: function(application, data){
@@ -174,6 +310,7 @@ var column = new Column({left:0, right:0, top:0, bottom:0, skin:new Skin({fill:"
 
 		//col.add(new Line({top:6, height: 45, left:0, right:0, name: "line", skin:new Skin({fill:"#939393"}), 
 		//contents:[makeLabel("No notifications!", l1));
+		//mainContainer,
 	}},
 	
 	
