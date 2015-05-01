1f1e968bfff3ccac34ec8d8970f03bedc0ea1ae5
Sean Murphy <smurph@berkeley.edu>
2015-04-30 18:02:37 -0700

    pooooooop backend stuff

---
+++ IvySimulator/simulator/analog.js
@@ -0,0 +1,60 @@
+//@module
+
+var PinsSimulators = require('PinsSimulators');
+
+var configure = exports.configure = function(configuration) {
+	this.pinsSimulator = shell.delegate("addSimulatorPart", {
+			header : { 
+				label : "Analog Senor of Levels", 
+				name : "Power, Water, Filter, Food", 
+				iconVariant : PinsSimulators.SENSOR_KNOB 
+			},
+			axes : [
+				new PinsSimulators.AnalogInputAxisDescription(
+					{
+						valueLabel : "Power",
+						valueID : "powerValue",
+						speed : 0.5
+					}
+				),
+				new PinsSimulators.AnalogInputAxisDescription(
+					{
+						valueLabel : "Water",
+						valueID : "waterValue",
+						speed : 0.5
+					}
+				),
+				new PinsSimulators.AnalogInputAxisDescription(
+					{
+						valueLabel : "Filter",
+						valueID : "filterValue",
+						speed : 0.5
+					}
+				),
+				new PinsSimulators.AnalogInputAxisDescription(
+					{
+						valueLabel : "Food",
+						valueID : "foodValue",
+						speed : 0.5
+					}
+				),
+			]
+		});
+}
+
+var close = exports.close = function() {
+	shell.delegate("removeSimulatorPart", this.pinsSimulator);
+}
+
+var read = exports.read = function() {
+	//var axes = this.pinsSimulator.delegate("getValue");
+		//return axes.analogValue;
+		return this.pinsSimulator.delegate("getValue");
+}
+
+exports.pins = {
+			power: { type: "A2D" },
+			water: { type: "A2D" },
+			filter: { type: "A2D" },
+			food: { type: "A2D" }
+		};
