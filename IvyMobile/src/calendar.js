fdc4408fc7aaa8d059ee733fd017614d2f8985cc
100kristine <kristine_y@ischool.berkeley.edu>
2015-05-06 10:06:55 -0700

    fixed cal

--- IvyMobile/src/calendar.js
+++ IvyMobile/src/calendar.js
@@ -4,6 +4,7 @@ var titlelabelStyle = new Style( { font: "70px Avenir", color:"white" } );
 var labelStyle = new Style( { font: "25px Avenir", color:"#868786" } );
 var whiteS = new Skin({fill:"white"});
 var SWITCHES = require('controls/switch');
+var POWERLEVEL = "fi";
 
 var grey = "#868786";
 var mint = "#95cfb0";
@@ -12,13 +13,14 @@ var mintSkin = new Skin({fill:mint ,borders:{left:2, right:2, top:2, bottom:2}, 
 var mintBorder = new Skin({fill:"white" ,borders:{left:2, right:2, top:2, bottom:2}, stroke:mint});
 
 var active = new Style( { font: "30px Avenir", color:"white" } );
+var activeDark = new Style( { font: "30px Avenir", color:grey } );
+var tiny = new Style( { font: "20px Avenir", color:"white" } );
 var inactive = new Style( { font: "30px Avenir", color:"white"} );
 var l2 = new Style( { font: "25px Avenir", color:grey, horizontal:"left"} );
 var bold = new Style( { font: "25px Avenir Black", color:"#1eaf5f", horizontal:"left"} );
 var bold2 = new Style( { font: "25px Avenir Black", color:"white"} );
 var l3 = new Style( { font: "70px Avenir", color:"white" } );
 //Backend sensors stuff
-
 //
 function makeLabel(str,sty){
   return new Label({left:5, right:5, top:10, height:50, string:str,style:sty,skin:whiteS});
@@ -102,15 +104,18 @@ function makeSkin(f){
 	return new Skin({fill:f ,borders:{left:2, right:2, top:2, bottom:2}, stroke:"white"})
 }
 
-var week = new Line({left:5, right:5, top:0, bottom:0,height:10,skin: mintSkin, contents:[]});
+var week = new Column({left:5, right:5, top:0, bottom:0,height:50,skin: whiteS, contents:[]});
 var sched = new Line({left:5, right:5, top:0, bottom:0,height:100,skin: whiteS, contents:[]});
 var scheduled = new Line({left:5, right:5, top:-60, bottom:0,height:0,skin: whiteS, contents:[]});
 
 //Today color, trim, filter, food ...
-var skins = [makeSkin("#f4edc1"),makeSkin("#1eaf5f"),makeSkin("#868786"),makeSkin("#ee5c85")]
-
+var skins = [makeSkin("#32c0da"),makeSkin("#1eaf5f"),makeSkin("#868786"),makeSkin("#ee5c85")]
+var things = "Today Trim Filter Food".split(" ");
 function makeBlock(colorIndex){
-	return new Column({left:5, right:5, top:10, height:20,skin:skins[colorIndex]});
+
+	return new Line({left:0, right:0, top:0,height:20,skin:skins[colorIndex], contents:[
+		new Label({left:0, right:0, top:0, height:20,string:things[colorIndex],style:tiny})
+	]});
 }
 
 function getItem(index,item){
@@ -119,33 +124,54 @@ function getItem(index,item){
 		item = item.next
 		index -= 1;
 	}
-	return item;
+	return item.first.next;
 }
 
 function populateWeek(){
-	weeks = "Su M Tu W Th F Sa".split(" ");
+	weeks = "Su M Tu W Th F Sa".split(" "); curr = 21;
 	for (i=0; i< weeks.length; i++) {
-		week.add(new Label({name:weeks[i], left:5, right:5, top:10, height:40, string:weeks[i],style:active,skin:mintSkin}));
+
+		//if (weeks[i] == "Su") {var sk = skins[0];}
+		//				else {var sk = mintBorder;}
+
+		week.add( new Line({left:0, right:0, top:0, bottom:0,height:60,
+			contents:[
+				new Column({left:0, right:0, top:0, bottom:0,height:100,
+					contents: [
+						//new Label({name:"May "+curr+i, left:5, right:5, top:10, height:40,string:weeks[i],style:active,skin:mintSkin}),
+						new Label({name:weeks[i], left:5, right:5, top:0, height:40,string:weeks[i],style:activeDark,skin:mintBorder})
+					]
+				}),
+				new Column({left:0, right:0, top:0, bottom:0,height:60,
+					contents: [
+						//new Label({name:weeks[i], left:5, right:5, top:10, height:40,string:weeks[i],style:active,skin:mintSkin})
+					]
+				}),
+			]})
+		)
+		
+		
+		
+			
 		var temp = new Column({left:5, right:5, top:10, height:20,skin:whiteS})
 		
 		sched.add(temp);
 	}
 
-	var func = "Today Trim Filter Food".split(" ");
-	var s = new Style( { font: "25px Avenir", color:"white" } );
+	/*var s = new Style( { font: "25px Avenir", color:"white" } );
 	for (i=0; i< func.length; i++) {
-		scheduled.add(new Label({left:5, right:5, top:10, height:50, string:func[i],style:s,skin:skins[i]}));
-	};
+		scheduled.add(new Label({left:5, right:5, top:10, height:50, string:things[i],style:s,skin:skins[i]}));
+	};*/
 	
 	//Fix Me
 	//adds blocks to schedule 
 	//getItem: SU = 0, M = 1, etc....
 	//makeBlock(index of color, 0 = today, 1 = trim, 2 = filter, 3 = food)
-	getItem(0,sched).add(makeBlock(1));
-	getItem(4,sched).add(makeBlock(1));
-	getItem(4,sched).add(makeBlock(2));
-	getItem(0,sched).add(makeBlock(0));
-	getItem(5,sched).add(makeBlock(3));
+	getItem(4,week).add(makeBlock(1));
+	getItem(0,week).add(makeBlock(1));
+	getItem(4,week).add(makeBlock(2));
+	getItem(0,week).add(makeBlock(0));
+	getItem(6,week).add(makeBlock(3));
 }
 
 populateWeek();
@@ -182,16 +208,16 @@ var plantPicture = new Picture({ left:0, right:0, top:0, height:160, width:150,
 var plantPicture2 = new Picture({ left:0, right:0, top:0, bottom:0,height:160, width:150,
 									url: "herb.png" });
 
-var scheduleCol = new Column({left:0, right:0, top:0, bottom:-40,height:5, width:20,skin: whiteS,
+var scheduleCol = new Column({left:0, right:0, top:0, bottom:-10,height:200, width:20,skin: whiteS,
 						contents:[
-						new Label({left:5, right:5, top:20, height:40, string:"Schedule created by IVY Vase",style:bold2,skin:mintSkin}),
-						new Column({left:0, right:0, top:0, bottom:0,height:140, width:20,skin: whiteS, contents:[
+						new Label({left:5, right:5, top:20, height:40, string:"Schedule Generated by IVY Vase",style:bold2,skin:mintSkin}),
+						new Column({left:0, right:0, top:0, bottom:0,height:200, width:20,skin: whiteS, contents:[
 							week,
-							sched,
-							scheduled
+							//sched,
+							//scheduled
 							]}),
-						new Column({left:0, right:0, top:0, bottom:0,height:5, width:20,skin: whiteS, contents:[
-							]})
+						//new Column({left:0, right:0, top:0, bottom:0,height:5, width:20,skin: whiteS, contents:[
+						//	]})
 						]})
 
 var vaseCol = new Column({left:0, right:0, top:0, bottom:-40,height:5, width:50,skin: mintBorder,
